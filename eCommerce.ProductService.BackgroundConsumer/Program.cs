3e2c9330b32203f3bcd4c5f2bb815fcdfda00b9f
Matías Araya <matiasaraya28@gmail.com>
2026-06-06 11:14:48 +0200

    remove consumer from api

--- eCommerce.ProductService.BackgroundConsumer/Program.cs
+++
@@ -1,9 +0,0 @@
-
-// Delete
-using eCommerce.ProductService.ProductConsumer;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
